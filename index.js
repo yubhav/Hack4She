ea6b67b504814934ab9b4b18293cd34256fe0f7a
Yubhav Seth <yubhav.seth97@gmail.com>
2020-11-20 05:44:26 +0530

    changes server

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-const express = require("express");
+const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const express = require("express");
+const admin = require("firebase-admin");
+
+
 app = express();
+
+
+app.engine("html", require("ejs").renderFile);
+
 app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/login", function (req, res) {
+    res.render("admin_signin.html");
+  });
+  
+  app.get("/signup", function (req, res) {
+    res.render("admin_signup.html");
+  });
+
+app.get("/", function (req, res) {
+    res.render("index.html");
+  });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server started at port ${port}`));
+app.listen(port, () => console.log(`Server started at port ${port}`)); 
