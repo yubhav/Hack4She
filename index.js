d52a4d2f54e34b4c73021beacdcceae1661305ad
Yubhav Seth <yubhav.seth97@gmail.com>
2020-11-22 01:57:22 +0530

    add gallery

--- index.js
+++ index.js
@@ -32,6 +32,10 @@ app.get("/", function (req, res) {
     res.render("index.html");
   });
 
+app.get("/gallery", function (req, res) {
+  res.render("gallery.html");
+});
+
 const userRouter = require('./routes/user');
 app.use('/',userRouter);
 
